5690d5ddd3544a96b5c4e0ba619bf69cc629c25b
Haiyi Zhong <haiyizxx@gmail.com>
2023-03-13 16:39:17 -0400

    update middleware sample

--- native-integration/multi-send/cosmos/gmp_handler.go
+++ native-integration/multi-send/cosmos/gmp_handler.go
@@ -3,5 +3,6 @@ package gmpdemo
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
 type GeneralMessageHandler interface {
+	HandleGeneralMessage(ctx sdk.Context, srcChain, srcAddress string, payload []byte, receiver string) error
 	HandleGeneralMessageWithToken(ctx sdk.Context, srcChain, srcAddress string, payload []byte, receiver string, coin sdk.Coin) error
 }

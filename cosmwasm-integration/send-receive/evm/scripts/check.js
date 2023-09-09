e1971a2357ab2072d1760be5ef4864e87af754fa
Milap Sheth <milap@axelar.network>
2023-09-09 00:43:56 -0400

    fix split

--- cosmwasm-integration/send-receive/evm/scripts/check.js
+++ cosmwasm-integration/send-receive/evm/scripts/check.js
@@ -8,7 +8,7 @@ async function main() {
   let SendReceiveFactory = await hre.ethers.getContractFactory("SendReceive");
   const contract = SendReceiveFactory.attach(process.env.CONTRACT || "0xcD9ce18C188B0befeE21601beE34be7Ce4cfe255", new JsonRpcProvider(process.env.RPC));
 
-  const [sender, message] = await contract.storedMessage().split(",");
+  const [sender, message] = (await contract.storedMessage()).split(",");
   console.log(`Received data:`);
   console.log(`Sender: ${sender}`);
   console.log(`Message: ${message}`);

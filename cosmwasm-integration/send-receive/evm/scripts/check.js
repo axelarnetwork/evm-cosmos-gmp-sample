e59d44a7c7aef44335e29463798a77fbeb75a5b5
Milap Sheth <milap@axelar.network>
2023-09-09 00:43:26 -0400

    show sender and message

--- cosmwasm-integration/send-receive/evm/scripts/check.js
+++ cosmwasm-integration/send-receive/evm/scripts/check.js
@@ -8,8 +8,10 @@ async function main() {
   let SendReceiveFactory = await hre.ethers.getContractFactory("SendReceive");
   const contract = SendReceiveFactory.attach(process.env.CONTRACT || "0xcD9ce18C188B0befeE21601beE34be7Ce4cfe255", new JsonRpcProvider(process.env.RPC));
 
-  const message = await contract.storedMessage();
-  console.log(`Received message: ${message}`);
+  const [sender, message] = await contract.storedMessage().split(",");
+  console.log(`Received data:`);
+  console.log(`Sender: ${sender}`);
+  console.log(`Message: ${message}`);
 }
 
 main().catch((error) => {

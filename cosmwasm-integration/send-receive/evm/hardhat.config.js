85ba8a383cb1a07c7d0c76a6fcf71e176932891d
Milap Sheth <milap@axelar.network>
2023-09-08 23:46:31 -0400

    add dummy private key

--- cosmwasm-integration/send-receive/evm/hardhat.config.js
+++ cosmwasm-integration/send-receive/evm/hardhat.config.js
@@ -15,7 +15,7 @@ module.exports = {
   networks: {
     onyx: {
       url: process.env.RPC,
-      accounts: [process.env.KEY],
+      accounts: [process.env.KEY || "0566b0ae7e92e8c645a80855c541b18be2d91858a8e4f7de82f7306666165a17"],  // test private key
     }
   }
 };
